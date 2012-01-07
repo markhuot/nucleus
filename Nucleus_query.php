c11f85d387a70db531447d1c2418f8b379223181
Mark Huot <mark@markhuot.com>
2012-01-06 21:38:07 -0500

    Update Nucleus_query.php

--- Nucleus_query.php
+++ Nucleus_query.php
@@ -310,14 +310,3 @@ class Nucleus_query {
 
 	
 }
-
-class Nucleus_join_one extends Nucleus_join {
-	public static check($config=array()) {
-		$join = new Nucleus_join_one(array_merge(array(
-			'primary_key' => $config['foreign_table'].'_id',
-			'foreign_key' => 'id'
-		), $config));
-
-		return $join->_check_join_columns()?$join:FALSE;
-	}
-}
