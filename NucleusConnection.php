33327e036a04c895f4c2c7bd743422d5c6db8a14
Mark Huot <mark@markhuot.com>
2012-01-07 14:50:28 -0500

    Updated to just extend PDO.

--- NucleusConnection.php
+++ NucleusConnection.php
@@ -2,35 +2,11 @@
 
 namespace Nucleus;
 
-class Connection {
+class Connection extends \PDO {
 	static $connections = array();
-	private $dsn;
-	private $user;
-	private $pass;
-
+	
 	public function __construct($dsn=NULL, $user=NULL, $pass=NULL) {
-		$this->connect($dsn, $user, $pass);
-	}
-
-	public function connect($dsn=NULL, $user=NULL, $pass=NULL) {
-		$this->dsn = $dsn?:$this->dsn;
-		$this->user = $user?:$this->user;
-		$this->pass = $pass?:$this->pass;
-
-		if (!$this->dsn) {
-			return FALSE;
-		}
-
-		$connection = new \PDO($this->dsn, $this->user, $this->pass);
-
-		if (!$connection) {
-			throw new \Exception('Connection error.');
-		}
-
-		return $this->connections[] = $connection;
-	}
-
-	public function connection() {
-		return $this->connection[count($this->connections)-1];
+		parent::__construct($dsn, $user, $pass);
+		self::$connections[] = $this;
 	}
 }
