f7559ff0ebe5150c9f1a39b14d60433b9e0c2ff2
Mark Huot <mark@markhuot.com>
2012-01-14 11:50:52 -0500

    adding tests for models

--- tests/05ModelTests.php
+++ tests/05ModelTests.php
@@ -3,8 +3,6 @@
 class ModelTests extends Quiz {
 	
 	public function __construct() {
-		define('APPPATH', __DIR__.'/../../../');
-		define('EXT', '.php');
 		$this->db = new Nucleus\Query(new Nucleus\Connection(
 			'mysql:host=192.168.94.31;dbname=tmp',
 			'root',
@@ -16,8 +14,21 @@ class ModelTests extends Quiz {
 		return Nucleus::config('model_path') !== FALSE;
 	}
 
+	public function anonymousModelLoaded() {
+		$model = Nucleus\Model::for_table('categories');
+		return get_parent_class($model) == 'Nucleus\Model';
+	}
+
 	public function modelLoaded() {
-		return Nucleus\Model::for_table('posts');
+		$model = Nucleus\Model::for_table('posts');
+		return get_class($model) == 'PostModel' && 
+		       get_parent_class($model) == 'Nucleus\Model';
+	}
+
+	public function joinFromModel() {
+		$this->db->from('posts');
+		$this->db->join('tagged');
+		$posts = $this->db->go();
 	}
 
 }
