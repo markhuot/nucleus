dfe091f55020560bff9dc46fbda8b09a045ee972
Mark Huot <mark@markhuot.com>
2012-01-14 01:38:33 -0500

    adding comments and beefing up the __call method to match the __get method

--- NucleusRecord.php
+++ NucleusRecord.php
@@ -54,14 +54,37 @@ class Record {
 		return $this->data;
 	}
 
-	public function __call($method, $args) {
-		if (isset($this->data[$method])) {
-			return $this->data[$method];
+	/**
+	 * Call
+	 *
+	 * There are two ways to access record properties, the first is through a
+	 * method call matching the key. So something like Post::title() would
+	 * look for the `title` column. Both are needed to account for the various
+	 * template languages (such as Mustache and Twig which both use different
+	 * access methodologies).
+	 */
+	public function __call($key, $args) {
+		
+		if (isset($this->data[$key])) {
+			return $this->data[$key];
+		}
+
+		if ($related = $this->result->related($key, $this)) {
+			return $related;
 		}
 
 		return FALSE;
 	}
 
+	/**
+	 * Get
+	 *
+	 * The second way to find a property is through direct access on the
+	 * object. This would happen like so: $post->title and would also look for
+	 * a `title` column. Both are needed to account for the various template
+	 * languages (such as Mustache and Twig which both use different access
+	 * methodologies).
+	 */
 	public function __get($key) {
 
 		if (isset($this->data[$key])) {
