873755909298dd9341b6c1e1e68946b7310c4e75
Mark Huot <mark@markhuot.com>
2012-01-14 11:50:41 -0500

    pulling a defined or anonymous model

--- NucleusModel.php
+++ NucleusModel.php
@@ -4,8 +4,24 @@ namespace Nucleus;
 
 class Model {
 
+	/**
+	 * Load Model for Table
+	 *
+	 * Takes a table name and loads the corresponding model file. If no model 
+	 * is defined we'll create an anomyous model on the author's behalf.
+	 */
 	public static function for_table($table) {
-		
+		$model_base_path = \Nucleus::config('model_path');
+		$model_name = ucfirst(singular($table)).'Model';
+		$model_path = "{$model_base_path}/".$model_name.'.php';
+		if (file_exists($model_path)) {
+			require_once $model_path;
+			return new $model_name;
+		}
+
+		else {
+			return new AnonymousModel;
+		}
 	}
 
 }
