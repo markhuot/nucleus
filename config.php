1d7831f72f84a11b0eac473bc4a543ddbdf5a591
Mark Huot <mark@markhuot.com>
2012-01-15 16:05:46 -0500

    adding default MAMP config

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 <?php
 
 $config['model_path'] = rtrim(__DIR__, '/').'/models';
+$config['dbtype'] = 'mysql';
+$config['dbsock'] = '/Applications/MAMP/tmp/mysql.sock';
+$config['dbname'] = 'tmp';
+$config['dbuser'] = 'root';
+$config['dbpass'] = 'root';
