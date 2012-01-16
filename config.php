92853d2831808003b58d59e5edc6e06020ec6b45
Mark Huot <mark@markhuot.com>
2012-01-15 22:20:41 -0500

    config update, less mamp-y

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
 $config['model_path'] = rtrim(__DIR__, '/').'/models';
 $config['dbtype'] = 'mysql';
 $config['dbhost'] = 'localhost';
-//$config['dbsock'] = '/Applications/MAMP/tmp/mysql/mysql.sock';
 $config['dbname'] = 'tmp';
 $config['dbuser'] = 'root';
 $config['dbpass'] = 'root';
