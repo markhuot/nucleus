04e0b234c12cbc141da135df0ff8bbe68719561f
Mark Huot <mark@markhuot.com>
2012-01-16 20:02:16 -0500

    added some initial documentation

--- Nucleus.php
+++ Nucleus.php
@@ -1,7 +1,22 @@
 <?php
 
+/**
+ * Nucleus is a zero-conf ORM.
+ */
+
+// Load in the helpers.
 require_once 'NucleusHelper.php';
 
+/**
+ * Set up the autoloader. This will be called anytime an undefined class within
+ * the \Nucleus namespace is requested. We'll then, quickly, require the
+ * appropriate file, defining our class.
+ * 
+ * The pattern is to turn underscores (_) to directory separators mash
+ * together namespaces. So, the class \Nucleus\Connection becomes
+ * NucleusQuery.php and the class \Nucleus\Connection_Mysql can be found in the
+ * NucleusConnection folder in a file named Mysql.php
+ */
 spl_autoload_register(function($class_name) {
 	if (substr($class_name, 0, 7) === 'Nucleus') {
 		$file_name = str_replace('_', '/', $class_name);
@@ -11,6 +26,27 @@ spl_autoload_register(function($class_name) {
 	}
 });
 
+/**
+ * The base class sits outside the `Nucleus` namespace. This is essentially a
+ * dummy class without much purpose beyond proxying requests to the `Query`
+ * object. This way you can utilize Nucleus without creating numerous
+ * `Nucleus\Query` objects.
+ * In other words, you can access Nucleus in two ways, the first is through
+ * this class.
+ * 
+ *      $nucleus = new \Nucleus;
+ *      $posts = $nucleus->get('posts');
+ *      $users = $nucleus->get('users');
+ * 
+ * The second way is through the query object directly.
+ * 
+ *     $query1 = new \Nucleus\Query;
+ *     $posts = $query1->get('posts');
+ *     
+ *     $query2 = new \Nucleus\Query;
+ *     $users = $query2->get('users');
+ * 
+ */
 class Nucleus {
 	private $query;
 	private static $config = array(
