ef7d5a327af75a621904ba5711919f69d10db6ea
Mark Huot <mark@markhuot.com>
2012-01-24 15:43:04 -0500

    updated again for local tests

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $config['model_path'] = rtrim(__DIR__, '/').'/models';
 $config['dbtype'] = 'mysql';
-$config['dbhost'] = 'localhost';
+$config['dbhost'] = '192.168.94.31';
 $config['dbname'] = 'tmp';
 $config['dbuser'] = 'root';
 $config['dbpass'] = 'root';
